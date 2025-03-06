d7377a5a119e8bdb1c3ee5f30dc23d11108508e0
Roman <romaletsuk@gmail.com>
2025-03-06 17:11:16 +0200

    FIO-9787: fixed issue when form module conflict with builder configuration (#23)

--- angular/src/projects/enterprise-builder/src/form/forms.service.ts
+++ angular/src/projects/enterprise-builder/src/form/forms.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { FormService } from '@formio/enterprise-builder-core';
+import { Formio } from '@formio/js/sdk';
 import { EnterpriseBuilderService } from '../enterprise-builder.service';
 
 @Injectable({
@@ -8,5 +9,34 @@ import { EnterpriseBuilderService } from '../enterprise-builder.service';
 export class FormsService extends FormService {
   constructor(public appService: EnterpriseBuilderService) {
     super(appService);
+  }
+
+  initializeFormModule() {
+    const currentProject = this.app.currentProject;
+    if (!currentProject.public.formModule) {
+        return null;
+    }
+
+    let formModule = null;
+    let builderOptions = null;
+    try {
+        formModule = Formio.Evaluator.evaluate(`return ${currentProject.public.formModule}`);
+    }
+    catch (err) {
+        console.warn(err);
+    }
+    if (formModule && formModule.options?.builder) {
+        if (this.builderOptions?.builder) {
+            builderOptions = {
+                ...(formModule.options?.builder?.builder ?? {}),
+                ...this.builderOptions.builder,
+            };
+            formModule.options.builder.builder = builderOptions;
+        }
+
+        Formio.use(formModule);
+    }
+
+    return builderOptions;
   }
 }
